564c4d6fef05081bcb5d9ee8e28931b14c56033b
youssef <youssef.essam@neom.com>
2024-10-21 10:07:05 +0300

    update

--- app/src/main/java/com/tcc/poc/feature/ui/fragments/home/TransactionAdapter.kt
+++ app/src/main/java/com/tcc/poc/feature/ui/fragments/home/TransactionAdapter.kt
@@ -1,13 +1,16 @@
 package com.tcc.poc.feature.ui.fragments.home
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.tcc.poc.R
 import com.tcc.poc.domain.models.Transaction
 import com.tcc.poc.domain.models.TransactionResponse
+import com.tcc.poc.feature.ui.fragments.util.formatDate
 
 class TransactionAdapter(private val transactionList: List<TransactionResponse>) :
     RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>() {
@@ -25,10 +28,11 @@ class TransactionAdapter(private val transactionList: List<TransactionResponse>)
         return TransactionViewHolder(view)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) {
         val transaction = transactionList[position]
         holder.transactionNumber.text = transaction.transactionNumber
-        holder.amount.text = transaction.amount.toString()
+        holder.amount.text =String.format("%.2f", transaction.amount!!.toDouble())
         holder.date.text = transaction.createdDate
         holder.vendor.text = transaction.business.name
     }
---
+++ app/src/main/java/com/tcc/poc/feature/ui/fragments/util/Extension.kt
@@ -0,0 +1,18 @@
+package com.tcc.poc.feature.ui.fragments.util
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun String.formatDate(): String {
+    // Parse the input string to LocalDateTime
+    val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS", Locale.getDefault())
+    val dateTime = LocalDateTime.parse(this, inputFormatter)
+
+    // Format it to the desired output
+    val outputFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.getDefault())
+    return dateTime.format(outputFormatter)
+}
